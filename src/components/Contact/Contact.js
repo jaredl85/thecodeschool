5b82418c57fd7b7e7147e4ab7e39fd50ac9e01a2
jaredl85 <jaredloveridge@gmail.com>
2021-12-28 13:39:29 -0800

    created contact form

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,10 +1,47 @@
-import React from 'react';
-import './contact.css';
+import React from "react";
+import "./contact.css";
 
 export default function Contact() {
-    return (
-        <div className="contact">
-            This is CONTACT
+  return (
+    <div className="contact">
+      <h2>Start Learning Now</h2>
+      <form className="contact-form">
+        <div className="form-group">
+          <div className="label-box">
+            <label className="form-label">Name:</label>
+          </div>
+          <input className="form-input" />
         </div>
-    )
+        <div className="form-group">
+          <div className="label-box">
+            <label className="form-label">Email:</label>
+          </div>
+
+          <input className="form-input" />
+        </div>
+        <div className="form-group">
+          <div className="label-box">
+            <label className="form-label">Phone:</label>
+          </div>
+          <input className="form-input" />
+        </div>
+        <div className="form-group">
+          <div className="label-box">
+            <label className="form-label">Program:</label>
+          </div>
+          <select className="form-input">
+            <option value="FT front-end">Full-Time Front-End Bootcamp</option>
+            <option value="FT back-end">Full-Time Back-End Bootcamp</option>
+            <option value="UX">Full-Time UX Bootcamp</option>
+            <option value="PT front-end">Part-Time Front-End Course</option>
+            <option value="PT back-end">Part-Time Back-End Course</option>
+            <option value="online">Online Full-Stack Course</option>
+          </select>
+        </div>
+          <div className="submit-btn-box">
+            <input type="submit" value="Submit" className="submit-btn" />
+        </div>
+      </form>
+    </div>
+  );
 }
